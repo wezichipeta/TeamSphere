8d22c4ccab9907be20ca8bde9ac1b7e8334ab6d8
Zoey  Chen <40010142+zoeychenn@users.noreply.github.com>
2024-02-17 00:20:05 -0800

    refine non-logged in messaging interface

--- messages.php
+++ messages.php
@@ -37,38 +37,41 @@
                 </div>
 
             </div>
-            <div class="col">
+            <?php if(is_user_logged_in()) {?>
+                <div class="col">
 
-                <h2>Private Chats</h2>
-                <form action="handle_new_chat.php" method="post">
-                    <div class="mb-3">
-                        <label for="messageBodyInput" class="form-label">Start a New Chat</label>
-                        <select class="form-select" name="userId" aria-label="Default select example">
-                            <?php foreach($friendUsers as $friendUser): ?>
+                    <h2>Private Chats</h2>
+                    <form action="handle_new_chat.php" method="post">
+                        <div class="mb-3">
+                            <label for="messageBodyInput" class="form-label">Start a New Chat</label>
+                            <select class="form-select" name="userId" aria-label="Default select example">
+                                <?php foreach($friendUsers as $friendUser): ?>
+                                    <li class="list-group-item">
+                                        <option value="<?=$friendUser['id']?>"><?=$friendUser['fullname']?> (<?=$friendUser['email']?>)</option>
+                                    </li>
+                                <?php endforeach; ?>
+                            </select>
+                        </div>
+                        <button type="submit" class="btn btn-primary">Start</button>
+                    </form>
+
+                    <div style="margin: 30px auto;">
+                        <ul class="list-group">
+                            <?php foreach($chats as $chat): ?>
                                 <li class="list-group-item">
-                                    <option value="<?=$friendUser['id']?>"><?=$friendUser['fullname']?> (<?=$friendUser['email']?>)</option>
+                                    <div>
+                                        <a href="chat.php?chat_id=<?=$chat['id']?>">
+                                            <?=$chat['name']?>
+                                        </a>
+                                    </div>
                                 </li>
                             <?php endforeach; ?>
-                        </select>
+                        </ul>
                     </div>
-                    <button type="submit" class="btn btn-primary">Start</button>
-                </form>
 
-                <div style="margin: 30px auto;">
-                    <ul class="list-group">
-                        <?php foreach($chats as $chat): ?>
-                            <li class="list-group-item">
-                                <div>
-                                    <a href="chat.php?chat_id=<?=$chat['id']?>">
-                                        <?=$chat['name']?>
-                                    </a>
-                                </div>
-                            </li>
-                        <?php endforeach; ?>
-                    </ul>
                 </div>
+            <?php }?>
 
-            </div>
         </div>
     </div>
     
